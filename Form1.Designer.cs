7947fbafbc958f8121d2cc8a62500ee35cfc6baa
Douglas Crole <fiddlesticks0002@gmail.com>
2020-08-27 00:01:49 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace DougVideoPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.video = new LibVLCSharp.WinForms.VideoView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.video)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // video
+            // 
+            this.video.BackColor = System.Drawing.Color.Black;
+            this.video.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.video.Location = new System.Drawing.Point(0, 24);
+            this.video.MediaPlayer = null;
+            this.video.Name = "video";
+            this.video.Size = new System.Drawing.Size(800, 426);
+            this.video.TabIndex = 0;
+            this.video.Text = "videoView1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.video);
+            this.Controls.Add(this.menuStrip1);
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.video)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private LibVLCSharp.WinForms.VideoView video;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,434 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using LibVLCSharp.Shared;
+
+namespace DougVideoPlayer
+{
+    public partial class Form1 : Form
+    {
+        //private IKeyboardMouseEvents m_GlobalHook;
+        public LibVLC _libVLC;
+        public MediaPlayer _mp;
+        public int StoredVolume = 40;
+        private string VideoPath = "";
+        private string[] args;
+        private Rectangle posTopLeft, posTopRight, posBottomLeft, posBottomRight;
+        private Rectangle _screenRectangle;
+        private Rectangle _windowCoordinatesRectangle;
+        private int mouseX, mouseY;
+        private bool CursorIsInWindow = false;
+        private bool FullScreenEnabled = false;
+        private Timer timer;
+
+        public Form1(string[] pargs)
+        {
+            args = pargs;
+
+            if (!DesignMode)
+            {
+                Core.Initialize();
+            }
+
+            InitializeComponent();
+            _libVLC = new LibVLC();
+            _mp = new MediaPlayer(_libVLC);
+
+            video.MediaPlayer = _mp;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Subscribe();
+            _screenRectangle = screenBoundsRectangle();
+            _windowCoordinatesRectangle = windowCoordinatesRectangle();
+
+            if (args.Length > 0 && File.Exists(args[0]))
+            {
+                Text = Path.GetFileNameWithoutExtension(args[0]);
+                VideoPath = $"file://{args[0].Replace("#", "%23")}";
+            }
+
+            timer = new Timer();
+            timer.Interval = 50;
+            timer.Tick += Timer_Tick;
+            timer.Enabled = true;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(out Point lpPoint);
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+
+            Point cursorPos;
+            GetCursorPos(out cursorPos);
+
+            mouseX = cursorPos.X;
+            mouseY = cursorPos.Y;
+
+            if (WindowState != FormWindowState.Maximized &&
+                _windowCoordinatesRectangle.Width < (_screenRectangle.Width / 2) &&
+                !(_windowCoordinatesRectangle.Height >= (_screenRectangle.Height / 1.5)))
+            {
+                if (mouseX >= Left && mouseX <= Bounds.Right && mouseY >= Top && mouseY <= Bounds.Bottom)
+                {
+                    if ((ModifierKeys & Keys.Shift) == Keys.None && !CursorIsInWindow)
+                    {
+                        MoveOutOfTheWay();
+                    }
+                    else
+                    {
+                        menuStrip1.Show();
+                        CursorIsInWindow = true;
+                    }
+                }
+                else
+                {
+                    CursorIsInWindow = false;
+                    menuStrip1.Hide();
+                }
+            }
+
+            timer.Enabled = true;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            CalculateScreenPositions();
+            Bounds = posBottomRight;
+            _screenRectangle = screenBoundsRectangle();
+            _windowCoordinatesRectangle = windowCoordinatesRectangle();
+
+            _mp.Volume = 60;
+            if (!string.IsNullOrEmpty(VideoPath))
+            {
+                menuStrip1.Hide();
+                _mp.Play(new Media(_libVLC, new Uri(VideoPath)));
+            }
+        }
+
+        public void Subscribe()
+        {
+            //m_GlobalHook = Hook.GlobalEvents();
+
+            //m_GlobalHook.MouseMove += M_GlobalHook_MouseMove;
+        }
+
+        public void Unsubscribe()
+        {
+            //m_GlobalHook.Dispose();
+        }
+
+        private Rectangle screenBoundsRectangle()
+        {
+            return Screen.FromControl(this).Bounds;
+        }
+
+        private Rectangle windowCoordinatesRectangle()
+        {
+            return RectangleToScreen(Bounds);
+        }
+        
+        /// <summary>
+        /// Set up the four rectangles that define the positions in all the corners of the current screen where we want our window to jump to
+        /// </summary>
+        private void CalculateScreenPositions()
+        {
+            int MarginTop = 25, MarginBottom = 35, MarginLeft = 25, MarginRight = 25;
+
+            posTopLeft = new Rectangle(new Point(MarginLeft, MarginTop), new Size(Width, Height));
+            posTopRight = new Rectangle(new Point(_screenRectangle.Width - Width - MarginRight, MarginTop), new Size(Width, Height));
+            posBottomLeft = new Rectangle(new Point(MarginLeft, _screenRectangle.Height - Height - MarginBottom), new Size(Width, Height));
+            posBottomRight = new Rectangle(new Point(_screenRectangle.Width - Width - MarginRight, _screenRectangle.Height - Height - MarginBottom), new Size(Width, Height));
+        }
+
+        /// <summary>
+        /// Calculate the distance between 2 points, given the x and y coordinates of both points.
+        /// </summary>
+        /// <returns></returns>
+        private double GetDistance(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+        }
+
+        public struct RectangleAndDistance
+        {
+            public Rectangle rectangle;
+            public double distance;
+        }
+
+        public Rectangle GetNearestScreenPosRectangle()
+        {
+            List<RectangleAndDistance> rectangleAndDistances = new List<RectangleAndDistance>();
+            rectangleAndDistances.Add(new RectangleAndDistance { rectangle = posTopLeft, distance = DistanceFromPos(posTopLeft) });
+            rectangleAndDistances.Add(new RectangleAndDistance { rectangle = posTopRight, distance = DistanceFromPos(posTopRight) });
+            rectangleAndDistances.Add(new RectangleAndDistance { rectangle = posBottomLeft, distance = DistanceFromPos(posBottomLeft) });
+            rectangleAndDistances.Add(new RectangleAndDistance { rectangle = posBottomRight, distance = DistanceFromPos(posBottomRight) });
+
+            rectangleAndDistances = rectangleAndDistances.OrderBy(x => x.distance).ToList();
+            return rectangleAndDistances.First().rectangle;
+        }
+
+        public double DistanceFromPos(Rectangle posRectangle)
+        {
+            return GetDistance(_windowCoordinatesRectangle.Left, _windowCoordinatesRectangle.Top, posRectangle.Left, posRectangle.Top);
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            _screenRectangle = screenBoundsRectangle();
+            _windowCoordinatesRectangle = windowCoordinatesRectangle();
+            CalculateScreenPositions();
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Pause & Resume
+            if (e.KeyChar == ' ')
+            {
+                if (_mp.IsPlaying)
+                {
+                    _mp.Pause();
+                }
+                else
+                {
+                    _mp.Play(); 
+                }
+            }
+
+            // Mute
+            if (e.KeyChar == 'm')
+            {
+                if (_mp.Volume != 0)
+                {
+                    StoredVolume = _mp.Volume;
+                    _mp.Volume = 0;
+                }
+                else
+                {
+                    _mp.Volume = StoredVolume;
+                }
+            }
+
+            // Volume up
+            if (e.KeyChar == '+')
+            {
+                _mp.Volume += 10;
+            }
+
+            // Volume down
+            if (e.KeyChar == '-')
+            {
+                _mp.Volume -= 10;
+            }
+
+            // Back 10 seconds
+            if (e.KeyChar == '5')
+            {
+                JumpMilliseconds(-10000);
+            }
+
+            // Forward 10 seconds
+            if (e.KeyChar == '6')
+            {
+                JumpMilliseconds(10000);
+            }
+
+            // Back 30 seconds
+            if (e.KeyChar == '4')
+            {
+                JumpMilliseconds(-30000);
+            }
+
+            // Forward 30 seconds
+            if (e.KeyChar == '7')
+            {
+                JumpMilliseconds(30000);
+            }
+
+            // Back 5 minutes
+            if (e.KeyChar == '3')
+            {
+                JumpMilliseconds(-300000);
+            }
+
+            // Forward 5 minutes
+            if (e.KeyChar == '8')
+            {
+                JumpMilliseconds(300000);
+            }
+
+            // Back 15 minutes
+            if (e.KeyChar == '2')
+            {
+                JumpMilliseconds(-900000);
+            }
+
+            // Forward 15 minutes
+            if (e.KeyChar == '9')
+            {
+                JumpMilliseconds(900000);
+            }
+
+            // Back 1 hour
+            if (e.KeyChar == '1')
+            {
+                JumpMilliseconds(-3600000);
+            }
+
+            // Forward 1 hour
+            if (e.KeyChar == '0')
+            {
+                JumpMilliseconds(3600000);
+            }
+
+            // Full screen toggle
+            if (e.KeyChar == 'f')
+            {
+                ToggleFullScreenMode();
+
+            }
+
+            // Increase transparency
+            if (e.KeyChar == '[')
+            {
+                if (Opacity >= 0.1)
+                {
+                    Opacity -= 0.1;
+                }
+            }
+
+            // Increace opacity
+            if (e.KeyChar == ']')
+            {
+                if (Opacity <= 0.9)
+                {
+                    Opacity += 0.1;
+                }
+            }
+        }
+
+        private void JumpMilliseconds(int milliseconds)
+        {
+            long targetTime = _mp.Time + milliseconds;
+            if (targetTime > _mp.Length)
+            {
+                if (_mp.Length > 20000)
+                {
+                    targetTime = _mp.Length - 10000;
+                }
+                else
+                {
+                    targetTime = _mp.Length - 1;
+                }
+            }
+
+            if (targetTime < 1)
+            {
+                targetTime = 1;
+            }
+
+            _mp.Time = targetTime;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Unsubscribe();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Move(object sender, EventArgs e)
+        {
+            _screenRectangle = screenBoundsRectangle();
+            _windowCoordinatesRectangle = windowCoordinatesRectangle();
+        }
+
+        private void M_GlobalHook_MouseMove(object sender, MouseEventArgs e)
+        {
+            //mouseX = e.X;
+            //mouseY = e.Y;
+
+            //if (WindowState == FormWindowState.Maximized || _windowCoordinatesRectangle.Width >= (_screenRectangle.Width / 2)
+            //                                             || _windowCoordinatesRectangle.Height >= (_screenRectangle.Height / 1.5))
+            //{
+            //    return;
+            //}
+
+            //if (mouseX >= Left && mouseX <= Bounds.Right && mouseY >= Top && mouseY <= Bounds.Bottom)
+            //{
+            //    if ((ModifierKeys & Keys.Shift) == Keys.None && !CursorIsInWindow)
+            //    {
+            //        MoveOutOfTheWay();
+            //    }
+            //    else
+            //    {
+            //        menuStrip1.Show();
+            //        CursorIsInWindow = true;
+            //    }
+            //}
+            //else
+            //{
+            //    CursorIsInWindow = false;
+            //    menuStrip1.Hide();
+            //}
+        }
+
+        private void MoveOutOfTheWay()
+        {
+            Rectangle nearestRectangle = GetNearestScreenPosRectangle();
+            if (nearestRectangle.Equals(posBottomRight))
+            {
+                Bounds = posTopRight;
+            }
+
+            if (nearestRectangle.Equals(posTopRight))
+            {
+                Bounds = posTopLeft;
+            }
+
+            if (nearestRectangle.Equals(posTopLeft))
+            {
+                Bounds = posBottomLeft;
+            }
+
+            if (nearestRectangle.Equals(posBottomLeft))
+            {
+                Bounds = posBottomRight;
+            }
+
+            _screenRectangle = screenBoundsRectangle();
+            _windowCoordinatesRectangle = windowCoordinatesRectangle();
+        }
+
+        public void ToggleFullScreenMode()
+        {
+            if (FullScreenEnabled)
+            {
+                FormBorderStyle = FormBorderStyle.Sizable;
+                WindowState = FormWindowState.Normal;
+                FullScreenEnabled = false;
+            }
+            else
+            {
+                FormBorderStyle = FormBorderStyle.None;
+                WindowState = FormWindowState.Maximized;
+                FullScreenEnabled = true;
+            }
+        }
+        
+    }
+}
